21d67727f6e10778e518b5416730513cb9871245
Vrishabh-D-B <bansodvrishabh@pm.me>
2021-12-02 22:39:41 +0530

    PTC

--- splitString.cpp
+++ splitString.cpp
@@ -1,7 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+string split(int i, int j, string str){
+    string str1 = "";
+    for( i ; i < j; i++)
+        str1 += str[i];
+    return str1;
+}
 int main(){
-    string str = "";
-    char ch = 'a';
-    cout << str + ch <<"\n";
+    int i = 0, j = 0;
+    vector<string>res;
+    string str ;//= "Hello guys";
+    getline(cin,str);
+    while( j < str.length() ){
+        if(str[j] == ' '){
+            string str2 = split( i, j, str);
+            res.push_back(str2);
+            i = j;
+            i++;
+            j++;
+        }
+        j++;
+    }
+    res.push_back(split(i,j,str));
+    for(auto element : res)
+        cout << element << "\n";    
 }
